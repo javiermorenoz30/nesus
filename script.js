b24059a69dee11c32d7661a83e57e33e482511f6
javiermorenoz30 <84947673+javiermorenoz30@users.noreply.github.com>
2026-08-24 02:14:46 -0400

    Add website interactions

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('button').forEach(button=>{button.addEventListener('mouseenter',()=>button.style.transform='translateY(-3px)');button.addEventListener('mouseleave',()=>button.style.transform='translateY(0)')});
